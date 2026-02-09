38480647f8e8bc33dc0054e4d668fcd910f3b7d7
CyborgX-hash <sakshamsontakke713@gmail.com>
2026-02-09 11:54:28 +0530

    added frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,13 @@ const bookingRoutes = require("./routes/booking.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  }
+));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
